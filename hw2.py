c02837a090f39ec806dfbea3fd6a0e31e8e2efd2
Krish <krushers@gmail.com>
2014-04-28 20:06:21 -0500

    optimize LCV

--- hw2.py
+++ hw2.py
@@ -110,6 +110,9 @@ def getEmptySquareMRVAndMCV(cb):
 
 def leastConstrainingValue(row,col,cb):
 	lVals = cb.Constraints[row][col].legalVals
+	if len(lVals)==1:
+		return {0 : lVals[0]}
+
 	size=cb.BoardSize
 	subsquare = int(math.sqrt(size))
 	lcvDict = {}
@@ -119,16 +122,16 @@ def leastConstrainingValue(row,col,cb):
 	return lcvDict
 
 def constrainScore(row,col,val,cb,size):
-	tempLCV=0
+	score=0
 	subsquare = int(math.sqrt(size))
 	for i in range(size):
 		if i!=col:
-			if cb.Constraints[row][i].isAssigned==False and  val in cb.Constraints[row][i].legalVals:
-				tempLCV+=len(cb.Constraints[row][i].legalVals)-1
+			if not cb.Constraints[row][i].isAssigned and  val in cb.Constraints[row][i].legalVals:
+				score+=len(cb.Constraints[row][i].legalVals)-1
 	for j in range(size):
 		if j !=row:
-			if cb.Constraints[j][col].isAssigned==False and  val in cb.Constraints[j][col].legalVals:
-				tempLCV+=len(cb.Constraints[j][col].legalVals)-1
+			if not cb.Constraints[j][col].isAssigned and  val in cb.Constraints[j][col].legalVals:
+				score+=len(cb.Constraints[j][col].legalVals)-1
 	squareRow = row // subsquare
 	squareCol = col // subsquare
 	for i in range(subsquare):
@@ -136,9 +139,9 @@ def constrainScore(row,col,val,cb,size):
 			x = squareRow*subsquare+i
 			y = squareCol*subsquare+j
 			if x!= row and y!=col:
-				if cb.Constraints[x][y].isAssigned==False and val in cb.Constraints[x][y].legalVals:
-					tempLCV+=len(cb.Constraints[row][i].legalVals)-1
-	return tempLCV
+				if not cb.Constraints[x][y].isAssigned and val in cb.Constraints[x][y].legalVals:
+					score+=len(cb.Constraints[row][i].legalVals)-1
+	return score
 
 ############
 ##########
@@ -153,7 +156,8 @@ def backTrackingWithMRVandMCVandLCV(Sudokuboard,SudokuConstraintBoard,variable_a
 		(row,col) = getEmptySquareMRVAndMCV(SudokuConstraintBoard)
 		legalVals = SudokuConstraintBoard.Constraints[row][col].legalVals
 		lcvDict = leastConstrainingValue(row,col,SudokuConstraintBoard)
-		for i in sorted(lcvDict.iterkeys(),reverse=True):
+		sortedKeys = sorted(lcvDict.iterkeys(),reverse=True)
+		for i in sortedKeys:
 			variable_assign +=1
 			Sudokuboard = Sudokuboard.set_value(row,col,lcvDict[i])
 			myCopyOfCBoard = copy.deepcopy(SudokuConstraintBoard)
